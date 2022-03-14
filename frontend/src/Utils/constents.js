2721977e0a697ae3041a9fd61cbe46d6798b0865
veekshith-gandhi <veekshithgandhi@gmail.com>
2022-03-14 15:09:45 +0530

    heroku

--- frontend/src/Utils/constents.js
+++ frontend/src/Utils/constents.js
@@ -1,3 +1,4 @@
 /** @format */
 
-export const API_ROOT = "http://localhost:8000";
+// export const API_ROOT = "http://localhost:8000";
+export const API_ROOT = "https://trello-clone-web.herokuapp.com";
